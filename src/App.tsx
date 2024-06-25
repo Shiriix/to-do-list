3481650cfe8c6d8a702628e12e393e256fe11aeb
unknown <solomonpipi1@gmail.com>
2024-06-25 13:10:37 +0100

    created event handlers to handle change events

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC, useState } from "react";
 import "./app.css";
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todolist, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="Deadline " />
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline "
+            name="deadline"
+            onChange={handleChange}
+          />
         </div>
         <button> Add Task </button>
       </div>
